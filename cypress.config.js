b0ccdd233649409a2d9c8ca120a8fc82383502fb
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-06-15 00:58:12 -0300

    Deleção de arquivos padrão e iniciado gerar HTML de report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawsome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      //require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
